2cec897954390166809299bcad9131d94b39093e
fasiondog <fasiondog@163.com>
2020-02-13 19:59:26 +0800

    update

---
+++ pkg/hdf5_D.pkg/xmake.lua
@@ -0,0 +1,35 @@
+ 
+-- the hdf5 package
+option("hdf5_D")
+
+    -- show menu
+    set_showmenu(true)
+
+    -- set category
+    set_category("package")
+
+    -- set description
+    set_description("The hdf5 package")
+
+    -- set language: c99, c++11
+    set_languages("c99", "cxx11")
+
+    -- add defines to config.h if checking ok
+    add_defines_h("$(prefix)_PACKAGE_HAVE_HDF5")
+
+    -- add link directories
+    add_linkdirs("lib/$(mode)/$(plat)/$(arch)")
+
+    add_defines("H5_BUILT_AS_DYNAMIC_LIB")
+    
+    -- add links for checking
+    add_links("hku_hdf5_D")
+    add_links("hku_hdf5_hl_D")
+    add_links("hku_hdf5_cpp_D")
+    add_links("hku_hdf5_hl_cpp_D")
+
+    -- add c includes for checking
+    --add_cincludes("H5Cpp.h")
+
+    -- add include directories
+    add_includedirs("inc/$(mode)/$(plat)/$(arch)", "inc")
