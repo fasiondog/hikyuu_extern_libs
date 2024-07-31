e5dc8e5577a4bb1b1356ef4226cf4b06448f0d4a
fasiondog <fasiondog@163.com>
2024-07-31 18:17:16 +0800

    update

--- packages/h/hku_rest/xmake.lua
+++ packages/h/hku_rest/xmake.lua
@@ -3,9 +3,9 @@ package("hku_rest")
     set_homepage("https://github.com/fasiondog/hku_rest.git")
     set_description("C++ http server Library of Hikyuu.")
 
-    add_urls("https://github.com/fasiondog/hku_rest/archive/refs/tags/$(version).zip",
+    add_urls("https://github.com/fasiondog/hku_rest/archive/refs/tags/$(version).tar.gz",
              "https://github.com/fasiondog/hku_rest.git")    
-    add_versions("1.0.0", "a3722f67d6c54b162bb1e3b366fd81846e580c2f3f15c4d78f53634d035c45a4")
+    add_versions("1.0.0", "141999bf74ea3cf52f823149561ed0c631b8807bfeae4ccd34bd6c0f2f44ddc9")
 
     for _, name in ipairs({"mysql"}) do
         add_configs(name, {description = "Enable the " .. name .. " module.", default = true, type = "boolean"})
