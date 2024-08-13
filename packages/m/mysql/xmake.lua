ac909226835e1dcbe7ea080650c222654ec18065
fasiondog <fasiondog@163.com>
2024-08-13 12:37:04 +0800

    update

--- packages/m/mysql/xmake.lua
+++ packages/m/mysql/xmake.lua
@@ -15,6 +15,8 @@ package("mysql")
         add_urls("https://github.com/fasiondog/hikyuu_extern_libs/releases/download/1.0.0/mysql-$(version)-linux-aarch64.zip",
                  "https://gitee.com/fasiondog/hikyuu_extern_libs/releases/download/1.0.0/mysql-$(version)-linux-aarch64.zip")
         add_versions("8.0.21", "385a7e280f86aa864f02ae7061c940a20f1ace358f906d330453992331b638c8")
+    elseif is_plat("macosx") then
+        add_extsources("brew::mysql-client")
     end
 
     on_install("windows", "linux", "cross", function (package)
@@ -23,5 +25,17 @@ package("mysql")
         if package:is_plat("windows") then
             os.cp("bin", package:installdir())
         end
+    end)
+
+    on_load("macosx", function(package)
+        package:add("includedirs", "/usr/local/opt/mysql-client/include/mysql")
+        package:add("includedirs", "/usr/local/opt/mysql-client/include")
+        package:add("linkdirs", "/usr/local/opt/mysql-client/lib")
+        package:add("rpathdirs", "/usr/local/opt/mysql-client/lib")
+        package:add("links", "mysqlclient")
+    end)
+
+    on_install("macosx", function(package)
+        package:addenv("PATH", "/usr/local/opt/mysql-client/bin")
     end)
 
