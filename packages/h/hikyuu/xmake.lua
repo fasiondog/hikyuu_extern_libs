e7fca4c5c4bd59ff10199da055dc5c806a89a567
fasiondog <fasiondog@163.com>
2024-10-09 10:42:34 +0800

    update

--- packages/h/hikyuu/xmake.lua
+++ packages/h/hikyuu/xmake.lua
@@ -8,7 +8,7 @@ package("hikyuu")
              "https://gitee.com/fasiondog/hikyuu_extern_libs/releases/download/hikyuu/hikyuu-$(version).zip",
              "https://github.com/fasiondog/hikyuu.git",
              "https://gitee.com/fasiondog/hikyuu.git")
-    add_versions("2.2.1", "4238d2180a55a4a2f4ec888669aabaf16d7fc5a880a9ef50961efc9995306b6c")             
+    add_versions("2.2.1", "56435bc140a033ffc6374f17440ad175b13b9591211b6b0d5a7a7c4cfda1f285")             
 
     add_configs("hdf5",  { description = "Enable hdf5 kdata engine.", default = true, type = "boolean"})
     add_configs("mysql",  { description = "Enable mysql kdata engine.", default = true, type = "boolean"})
