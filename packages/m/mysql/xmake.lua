887eb83e64eab5e9c29735e58d37125b43c012c3
fasiondog <fasiondog@163.com>
2024-11-19 02:52:26 +0800

    update

--- packages/m/mysql/xmake.lua
+++ packages/m/mysql/xmake.lua
@@ -15,7 +15,7 @@ package("mysql")
         add_urls("https://github.com/fasiondog/hikyuu_extern_libs/releases/download/1.0.0/mysql-$(version)-linux-aarch64.zip",
                  "https://gitee.com/fasiondog/hikyuu_extern_libs/releases/download/1.0.0/mysql-$(version)-linux-aarch64.zip")
         add_versions("8.0.21", "385a7e280f86aa864f02ae7061c940a20f1ace358f906d330453992331b638c8")
-    elseif is_plat("macosx") and is_arch("aarch64", "arm64.*") then
+    elseif is_plat("macosx") and is_arch("arm64.*") then
         add_urls("https://gitee.com/fasiondog/hikyuu_extern_libs/releases/download/1.0.0/mysql-$(version)-macosx-arm64.zip")
         add_versions("8.0.40", "b7cd5ac0fba457abfeca57c53f39fe43166bead047afdb59f6a6f2258eeaec2c")
     end
