7ae56fb8317777c26763e6f6b08cf1ff804443db
fasiondog <fasiondog@163.com>
2024-11-19 02:16:09 +0800

    add macosx hdf5

--- packages/h/hdf5/xmake.lua
+++ packages/h/hdf5/xmake.lua
@@ -17,6 +17,16 @@ package("hdf5")
         add_urls("https://github.com/fasiondog/hikyuu_extern_libs/releases/download/1.0.0/hdf5-$(version)-linux-aarch64.zip",
                  "https://gitee.com/fasiondog/hikyuu_extern_libs/releases/download/1.0.0/hdf5-$(version)-linux-aarch64.zip")
         add_versions("1.12.2", "d73a880d9dfede0d5db1e30555fa251ca82efa437a0d93b46f5e64e87e71fc63")
+    elseif is_plat("macosx") then
+        add_urls("https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-$(version).tar.gz", {version = function (version)
+            return format("%d.%d/hdf5-%s/src/hdf5-%s", version:major(), version:minor(), version, version)
+        end})
+        add_versions("1.12.2", "2a89af03d56ce7502dcae18232c241281ad1773561ec00c0f0e8ee2463910f14")
+    end
+
+    
+    if is_plat("macosx") then
+        add_deps("cmake")
     end
 
     on_load("windows", "linux", "cross", function (package)
@@ -31,3 +41,40 @@ package("hdf5")
         end
     end)
 
+    on_install("macosx", function (package)
+        io.replace("CMakeLists.txt", 'set (HDF5_EXTERNAL_LIB_PREFIX "" CACHE STRING "Use prefix for custom library naming.")', 
+                   'set (HDF5_EXTERNAL_LIB_PREFIX "hku_" CACHE STRING "Use prefix for custom library naming.")', {plain = true})
+        local configs = {
+            "-DHDF5_GENERATE_HEADERS=OFF",
+            "-DBUILD_TESTING=OFF",
+            "-DHDF5_BUILD_EXAMPLES=OFF",
+            "-DHDF_PACKAGE_NAMESPACE:STRING=hdf5::",
+            "-DHDF5_MSVC_NAMING_CONVENTION=OFF",
+            "-DBUILD_SHARED_LIBS=ON",
+            "-DONLY_SHARED_LIBS=ON",
+            "-DBUILD_STATIC_LIBS=OFF",
+            "-DHDF5_BUILD_CPP_LIB=ON",
+            "-DALLOW_UNSUPPORTED=ON",
+            "-DHDF5_ENABLE_THREADSAFE=ON"
+        }
+        import("package.tools.cmake").install(package, configs)
+        package:addenv("HDF5_ROOT", path.join(package:installdir("cmake")))
+        package:addenv("PATH", package:installdir("bin"))
+    end)
+
+    on_test("macosx", function (package)
+        if package:config("shared") then
+            os.vrun("h5diff-shared --version")
+        else
+            os.vrun("h5diff --version")
+        end
+        assert(package:has_cfuncs("H5open", {includes = "H5public.h"}))
+
+        if package:config("cpplib") then
+             assert(package:check_cxxsnippets({test = [[
+                void test() {
+                    H5::H5Library::open();
+                }
+            ]]}, {configs = {languages = "c++17"}, includes = {"H5Cpp.h"}}))
+        end
+    end)
