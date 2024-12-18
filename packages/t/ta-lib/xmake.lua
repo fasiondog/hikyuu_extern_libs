72233aa6a865de7982d975d3578ee3dddc419c85
fasiondog <fasiondog@163.com>
2024-12-19 01:31:37 +0800

    update ta-lib (not support dll)

--- packages/t/ta-lib/xmake.lua
+++ packages/t/ta-lib/xmake.lua
@@ -6,23 +6,12 @@ package("ta-lib")
     add_urls("https://github.com/TA-Lib/ta-lib.git",
              "https://gitcode.com/KongDong/ta-lib.git")
 
-
     add_deps("cmake >=3.30")
 
-    on_load(function (package)
-        if package:config("shared") then
-            package:add("defines", "TA_LIB_API=__declspec(dllimport)")
-        end
-    end)
-
     on_install(function (package)
-        if package:config("shared") then
-            io.replace("CMakeLists.txt", "install(TARGETS ta-lib ta-lib-static", "install(TARGETS ta-lib", {plain = true})
-        else
-            io.replace("CMakeLists.txt", "install(TARGETS ta-lib ta-lib-static", "install(TARGETS ta-lib-static", {plain = true})
-        end
-        local configs = {"-DBUILD_DEV_TOOLS=OFF"}
-        import("package.tools.cmake").install(package, configs)
+        io.replace("CMakeLists.txt", "install(TARGETS ta-lib ta-lib-static", "install(TARGETS ta-lib-static", {plain = true})
+        io.replace("CMakeLists.txt", "DESTINATION include/ta-lib", "DESTINATION include", {plain = true})
+        import("package.tools.cmake").install(package, {"-DBUILD_DEV_TOOLS=OFF"})   
     end)
 
     on_test(function (package)
@@ -31,5 +20,5 @@ package("ta-lib")
                 TA_Initialize();
                 TA_Shutdown();
             }
-        ]]}, {configs = {languages = "c++11"}, includes = "ta-lib/ta_libc.h"}))
+        ]]}, {configs = {languages = "c++11"}, includes = "ta_libc.h"}))
     end)
