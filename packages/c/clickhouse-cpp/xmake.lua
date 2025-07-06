0ea123bd1d28bed22f31533f33b3c2b302cf887b
fasiondog <fasiondog@163.com>
2025-07-07 02:00:10 +0800

    add clickhouse-cpp

---
+++ packages/c/clickhouse-cpp/xmake.lua
@@ -0,0 +1,25 @@
+package("clickhouse-cpp")
+
+    set_homepage("https://github.com/ClickHouse/clickhouse-cpp")
+    set_description("C++ client for ClickHouse.")
+    set_license("Apache-2.0 license ")
+
+    add_urls("https://github.com/ClickHouse/clickhouse-cpp.git")
+    add_versions("2025.06.24", "cae657a672ff09b715d7127b13eb25d63bea01d4")
+
+    add_deps("cmake")
+    on_install("windows", "macosx", "linux", "mingw@windows", function (package)
+        import("package.tools.cmake").install(package)
+        os.cp("contrib/absl/absl", package:installdir("include"))
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            #include <iostream>
+            //#include <clickhouse/client.h>
+            using namespace clickhouse;
+            void test() {
+                Client client(ClientOptions().SetHost("localhost"));
+            }
+        ]]}, {configs = {languages = "c++17"}, includes = "clickhouse/client.h"}))
+    end)
