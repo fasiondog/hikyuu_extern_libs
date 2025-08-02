d728279b710d5cc2104f41454d1a6e80b08f25ae
fasiondog <fasiondog@163.com>
2025-08-02 15:11:26 +0800

    upgrade pybind11 to 3.0.0

--- packages/p/pybind11/xmake.lua
+++ packages/p/pybind11/xmake.lua
@@ -7,6 +7,7 @@ package("pybind11")
 
     add_urls("https://github.com/pybind/pybind11/archive/$(version).zip",
              "https://github.com/pybind/pybind11.git")
+    add_versions("v3.0.0", "dfe152af2f454a9d8cd771206c014aecb8c3977822b5756123f29fd488648334")
     add_versions("v2.11.1", "b011a730c8845bfc265f0f81ee4e5e9e1d354df390836d2a25880e123d021f89")
     add_versions("v2.13.1", "a3c9ea1225cb731b257f2759a0c12164db8409c207ea5cf851d4b95679dda072")             
     add_versions("v2.13.6", "d0a116e91f64a4a2d8fb7590c34242df92258a61ec644b79127951e821b47be6") 
