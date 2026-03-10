a9aa8037de6a7cfbebe6f596de8268b9d9af6c87
fasiondog <fasiondog@163.com>
2026-03-11 00:57:49 +0800

    update hku_utils

--- packages/h/hku_utils/xmake.lua
+++ packages/h/hku_utils/xmake.lua
@@ -7,6 +7,7 @@ package("hku_utils")
              "https://gitcode.com/KongDong/hku_utils.git",
              "https://github.com/fasiondog/hku_utils.git")
 
+    add_versions("1.3.3", "7b618425c18e560d673dd0bae603a05cc453e00a05aa5453f1bccd718f950b5f")
     add_versions("1.3.2", "67f2579fbee2b4082e1c7bbf5544d1a2ccd41fa8ecda152b42c283505be87ce8")
     add_versions("1.3.1", "a88d7f77f29bffbf7c3003cc69c8080f64b54a982a62f18870f72616901bc341")
     add_versions("1.3.0", "fc0f0e6e49e591bf9822d4176f6a4191872aee118201dc439ca82544023f385c")
