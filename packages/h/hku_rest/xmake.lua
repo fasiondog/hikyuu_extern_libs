74e0d3a4cdd2eb7bc7ef9b5822badeb1a462dc97
fasiondog <fasiondog@163.com>
2026-05-15 03:00:48 +0800

    update hku_rest

--- packages/h/hku_rest/xmake.lua
+++ packages/h/hku_rest/xmake.lua
@@ -6,6 +6,7 @@ package("hku_rest")
     add_urls("https://github.com/fasiondog/hku_rest/archive/refs/tags/$(version).tar.gz",
              "https://github.com/fasiondog/hku_rest.git")    
     
+    add_versions("1.2.2", "ed0b061f5505f5252f77e9dd3b00fe57b10dd4687b5f5851fcbe95551d06aabb")
     add_versions("1.2.1", "9dc72a11083792276ac929832c469fe1021b4b642478ae7ea493bd7813458bb5")
     add_versions("1.2.0", "f81d8b2a8e047a4d638ae5d1da1a6aca56e7e39fe80af2f270b9375b5e18689e")
     add_versions("1.1.2", "6e660eb344cfafa270b939161a1750b77eb2236652bf82ec115d508e37d5bcd2") --nng
@@ -44,53 +45,35 @@ package("hku_rest")
             package:add("defines", "HKU_HTTPD_API=__declspec(dllimport)")
         end
 
-       local boost_config
-        if package:is_plat("windows") then
-            boost_config = {
-                system = false,
-                debug = package:is_mode("debug"),
-                configs = {
-                    shared = true,
-                    runtimes = get_config("runtimes"),
-                    multi = true,
-                    date_time = true,
-                    filesystem = false,
-                    property_tree= true, --mqtt need
-                    serialization = true,
-                    system = true,
-                    python = false,
-                    asio = true,
-                    beast = true,
-                    cmake = false,
-            }}
-        else
-            boost_config = {
-                system = false,
-                configs = {
-                    shared = true, -- is_plat("windows"),
-                    runtimes = get_config("runtimes"),
-                    multi = true,
-                    date_time = true,
-                    filesystem = false,
-                    serialization = true, --get_config("serialize"),
-                    system = true,
-                    python = false,
-                    -- 以下为兼容 arrow 等其他组件
-                    thread = true,   -- parquet need
-                    chrono = true,   -- parquet need
-                    charconv = true, -- parquet need
-                    atomic = true,
-                    container = true,
-                    math = true,
-                    regex = true,
-                    random = true,
-                    thread = true,
-                    asio = true,  
-                    beast = true,
-                    cmake = true,
-            }}
-        end
-        package:add("deps", "boost", {configs = boost_config})
+        -- local boost_config = {
+        --         system = false,
+        --         configs = {
+        --             shared = is_plat("windows"),
+        --             runtimes = get_config("runtimes"),
+        --             multi = true,
+        --             date_time = true,
+        --             filesystem = false,
+        --             serialization = true, --get_config("serialize"),
+        --             system = true,
+        --             python = false,
+        --             -- 以下为兼容 arrow 等其他组件
+        --             thread = true,   -- parquet need
+        --             chrono = true,   -- parquet need
+        --             charconv = true, -- parquet need
+        --             atomic = true,
+        --             container = true,
+        --             math = true,
+        --             regex = true,
+        --             random = true,
+        --             thread = true,
+        --             asio = true,  
+        --             beast = true,
+        --             mysql = true,
+        --             cmake = false,
+        --     }}
+
+        -- package:add("deps", "boost", {configs = boost_config})
+        -- package:add("requireconfs", "**.boost", {override = true, configs = boost_config}) 
     end)
 
     on_install(function (package)
